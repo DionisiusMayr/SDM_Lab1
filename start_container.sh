e6b6899a9219396daf23a98b58274f034a8bfaea
Dionisius Mayr <dionisiusom@gmail.com>
2024-03-09 23:19:48 +0100

    Parameterize start_container.sh script

--- start_container.sh
+++ start_container.sh
@@ -1,12 +1,18 @@
+#!/usr/bin/env bash
+
+set -u
+
 docker stop NEO4J
 docker rm NEO4J
+
+BASE_DIR=${1}
 
 docker run \
     --name NEO4J \
     -p 7474:7474 \
     -p 7687:7687 \
     --env=NEO4J_AUTH=none \
-    -v ~/bdma/upc/semantic_data_management/SDM_Lab1/neo4j/import:/var/lib/neo4j/import \
-    -v ~/bdma/upc/semantic_data_management/SDM_Lab1/neo4j/logs:/var/lib/neo4j/logs \
-    -v ~/bdma/upc/semantic_data_management/SDM_Lab1/neo4j/conf:/var/lib/neo4j/conf \
+    -v ${BASE_DIR}/SDM_Lab1/neo4j/import:/var/lib/neo4j/import \
+    -v ${BASE_DIR}/SDM_Lab1/neo4j/logs:/var/lib/neo4j/logs \
+    -v ${BASE_DIR}/SDM_Lab1/neo4j/conf:/var/lib/neo4j/conf \
     neo4j:5.17.0-community-bullseye
