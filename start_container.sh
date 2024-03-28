2d295b2a68bb4b29f05bc95f1b07e1ea7d80ece0
Dionisius Mayr <dionisiusom@gmail.com>
2024-03-28 20:48:47 +0100

    Add APOC variables

--- start_container.sh
+++ start_container.sh
@@ -15,4 +15,8 @@ docker run \
     -v ${BASE_DIR}/SDM_Lab1/neo4j/import:/var/lib/neo4j/import \
     -v ${BASE_DIR}/SDM_Lab1/neo4j/logs:/var/lib/neo4j/logs \
     -v ${BASE_DIR}/SDM_Lab1/neo4j/conf:/var/lib/neo4j/conf \
+    -e NEO4J_apoc_export_file_enabled=true \
+    -e NEO4J_apoc_import_file_enabled=true \
+    -e NEO4J_apoc_import_file_use__neo4j__config=true \
+    -e NEO4J_PLUGINS=\[\"apoc-extended\"\] \
     neo4j:5.17.0-community-bullseye
